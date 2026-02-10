d1788d8ea4ac484407a672bdab7a0220f36ef336
saadai113 <sabdullah2@babson.edu>
2026-02-10 13:02:01 -0500

    class 7

---
+++ code/s07.py
@@ -0,0 +1,25 @@
+import turtle
+t=turtle.turtle()
+t.speed(0)
+t.forward(100)
+t.left
+def draw_square(turtle_obj, size=100):
+    """Draw a square with the gien size"""
+    for _ in range(4):
+        turtle_obj.forward(size)
+        turtle_obj.left(90)
+
+def draw_spiral(t):
+    """"""
+    for i in range(36):
+        draw_square(t, 50)
+        t.left(10)
+
+def main():
+    t=turtle.Turtle()
+    t.speed(0)
+    #draw_square(t)
+    #draw_sqiare(t, size=50)
+    draw_spiral(t)
+    turtle.mainloop()
+
