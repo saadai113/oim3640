e04872fecef82b7eaa0a937745d0d56224d57c85
saadai113 <sabdullah2@babson.edu>
2026-04-08 12:45:14 -0400

    sq20lite.py

--- code/sqlitedemo.py
+++ code/sqlitedemo.py
@@ -1,18 +1,5 @@
 import sqlite3
 
-from code.stocks import db
-
-conn = sqlite3.connect('towns.db')
-conn.execute('''CREATE TABLE IF NOT EXISTS towns
-                (name TEXT, county TEXT, pop INTEGER)''')
-conn.execute('INSERT INTO towns VALUES (?, ?, ?)',
-             ('Boston', 'Suffolk', 675647))
-conn.commit()
-
-for row in conn.execute('SELECT * FROM towns'):
-    print(row)  # ('Boston', 'Suffolk', 675647)
-import sqlite3
-
 
 def create_portfolio_table():
     """Create a table to store stock portfolio data."""
